b853f77c01dd969b69efceef849c085abf0dd071
Honorat <honorathounyovi0@gmail.com>
2026-04-03 21:55:20 +0100

    Done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (parseInt(a) !== a || parseInt(b) !== b || a < 0 || b < 0 ) {
+        return "ERROR";
+    }
+
+    let result = 0;
+
+    if(a < b) {
+        for(a; a <= b; a++){
+            result += a;
+        }
+    } else if (a > b) {
+        for(b; b <= a; b++){
+            result += b;
+        }
+    } else {
+        return "ERROR"
+    }
+
+    return result;
 };
 
 // Do not edit below this line
