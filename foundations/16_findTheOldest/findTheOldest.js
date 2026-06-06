49ed23cf8fe3973951037753f74efea37465e83f
Honorat-0 <honorathounyovi0@gmail.com>
2026-06-06 20:05:52 +0100

    Solve the exercise 16-findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  return arr.reduce((acc, curr)=> {
+    const currentYear = new Date().getFullYear();
+    const accYear = (acc.yearOfDeath || currentYear) - acc.yearOfBirth;
+    const currYear = (curr.yearOfDeath || currentYear) - curr.yearOfBirth;
+    return accYear > currYear ? acc : curr;
+  })
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
