8abf9f87bf688b28f7019d23660bde1474908947
Honorat-0 <honorathounyovi0@gmail.com>
2026-06-06 10:32:32 +0100

    Solve of

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr)=> acc + curr, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr)=> acc * curr, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let arr = []
+	if(num == 0) {
+    return 1;
+  } else if (num >= 1) {
+    for(num; num >= 1; num--){
+      arr.push(num)
+    }
+  }
+  return multiply(arr);
+
 };
 
 // Do not edit below this line
